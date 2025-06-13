052f447ec6ca54373e84ee885cb930afa9e9c603
AleksanderPostrzednik <62178376+AleksanderPostrzednik@users.noreply.github.com>
2025-06-13 08:08:02 +0200

    Improve SWIFT code handling

--- src/main/java/com/swiftly/swiftly/exception/GlobalExceptionHandler.java
+++ src/main/java/com/swiftly/swiftly/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.swiftly.swiftly.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,6 +28,13 @@ public class GlobalExceptionHandler {
         Map<String, String> error = new HashMap<>();
         error.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMostSpecificCause().getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
